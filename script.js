8bdbb21baabac66101fb50508e22fcdf1b27740f
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-06-24 13:59:35 +0200

    Add modal close

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ const startGameBtn = document
 function displayModal() {
   const modal = document.querySelector("#modal-container");
   modal.style.display = "block";
+
+  window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      modal.setAttribute("close", "");
+      window.setTimeout(() => {
+        modal.style.display = "none";
+        modal.removeAttribute("close");
+      }, 600);
+    }
+  });
 }
