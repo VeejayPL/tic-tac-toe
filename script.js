cdb82d980d404d375a5e4b2ea2dc7353fb97408d
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-06-25 18:17:30 +0200

    Add reset board function

--- script.js
+++ script.js
@@ -38,6 +38,6 @@ function renderBoard() {
 }
 function resetBoard() {
   const field = document.querySelectorAll(".field");
-  field.forEach(field, () => (field.innerText = ""));
+  field.forEach((field) => (field.innerText = ""));
 }
 renderBoard();
