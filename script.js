de3a28e842e3bec114b2dae8b34bda7dbadc379e
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-07-01 12:02:44 +0200

    Refactor code to factory functions

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 const Player = (name, marker) => {
-  const sayHello = () =>
-    console.log(`Hello! My name is ${name} and playing under ${marker}`);
-  return { name, marker, sayHello };
+  const getName = () => name;
+  const getMarker = () => marker;
+  const info = () => console.log(`${name}: ${marker}`);
+  return { getName, getMarker, info };
 };
 
-const startGameBtn = document
-  .querySelector("#start-btn")
-  .addEventListener("click", displayModal);
-const resetButton = document
-  .querySelector("#reset-btn")
-  .addEventListener("click", resetBoard);
-
-function displayModal() {
+const displayController = (() => {
+  const newGameBtn = document.querySelector("#start-btn");
+  const resetButton = document.querySelector("#reset-btn");
   const modal = document.querySelector("#modal-container");
   const closeBtn = document.querySelector("#modal-btn-close");
-  modal.style.display = "block";
-  closeBtn.addEventListener("click", () => {
-    modal.setAttribute("close", "");
-    window.setTimeout(() => {
-      modal.style.display = "none";
-      modal.removeAttribute("close");
-    }, 600);
+
+  newGameBtn.addEventListener("click", (e) => {
+    gameboardDisplay.clear();
+    toggleModal();
   });
+
+  closeBtn.addEventListener("click", (e) => toggleModal());
+  resetButton.addEventListener("click", (e) => gameboardDisplay.reset());
+
   window.addEventListener("click", (e) => {
     if (e.target == modal) {
       modal.setAttribute("close", "");
@@ -31,22 +28,59 @@ function displayModal() {
       }, 600);
     }
   });
-}
 
-function renderBoard() {
-  for (let i = 0; i < 9; i++) {
-    const gameBoard = document.querySelector("#gameboard");
-    const field = document.createElement("div");
-    field.classList.add("field");
-    field.setAttribute("data-index", [i]);
-    field.addEventListener("click", (e) => {
-      e.target.innerText = "X";
-    });
-    gameBoard.appendChild(field);
-  }
-}
-function resetBoard() {
-  const field = document.querySelectorAll(".field");
-  field.forEach((field) => (field.innerText = ""));
-}
-renderBoard();
+  const toggleModal = () => {
+    if (modal.style.display === "block") {
+      modal.setAttribute("close", "");
+      window.setTimeout(() => {
+        modal.style.display = "none";
+        modal.removeAttribute("close");
+      }, 600);
+    } else {
+      modal.style.display = "block";
+    }
+  };
+
+  const writeToDom = (selector, message) => {
+    document.querySelector(selector).innerText = message;
+  };
+
+  return { writeToDom };
+})();
+
+const gameboardDisplay = (() => {
+  const render = () => {
+    if (document.querySelector("#gameboard").innerHTML !== "") return;
+    for (let i = 0; i < 9; i++) {
+      const gameBoard = document.querySelector("#gameboard");
+      const field = document.createElement("div");
+      field.classList.add("field");
+      field.setAttribute("data-index", [i]);
+      field.addEventListener("click", (e) => {
+        e.target.innerText = "X";
+      });
+      gameBoard.appendChild(field);
+    }
+  };
+  const clear = () => {
+    for (let i = 0; i < 9; i++) {
+      const gameBoard = document.querySelector("#gameboard");
+      const field = document.querySelector(".field");
+      gameBoard.removeChild(field);
+    }
+  };
+  const reset = () => {
+    const field = document.querySelectorAll(".field");
+    field.forEach((field) => (field.innerText = ""));
+  };
+
+  return { render, reset, clear };
+})();
+
+const modal = (() => {
+  const startBtn = document.querySelector("#modal-btn");
+})();
+
+const playerCreator = (() => {})();
+
+const gameboard = (() => {})();
